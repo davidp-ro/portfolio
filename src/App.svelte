a11b87b2ce1d870873c3519deae6ab5ab2e45399
David Pescariu <davidpescariu12@gmail.com>
2021-10-01 14:47:53 +0300

    feat: Footer

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
   import Navbar from "./components/navbar.svelte";
   import Hero from "./components/hero.svelte";
-import Intro from "./components/intro.svelte";
+  import Intro from "./components/intro.svelte";
+  import Footer from "./components/footer.svelte";
 </script>
 
 <main>
   <Navbar />
   <Hero />
   <Intro />
+  <Footer />
 </main>
