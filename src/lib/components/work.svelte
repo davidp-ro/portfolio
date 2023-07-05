56c2b128256cb3915b84cba70f8fee421a2812fc
David Pescariu <davidpescariu12@gmail.com>
2023-07-05 20:41:55 +0300

    feat: Open Source projects section

--- src/lib/components/work.svelte
+++ src/lib/components/work.svelte
@@ -1,5 +1,6 @@
 <script>
   import Visibility from "$lib/components/visibility.svelte";
+  import Projects from "./projects.svelte";
   import WorkCard from "./workCard.svelte";
 
   let isVisible = false;
@@ -29,7 +30,7 @@
 
   <!-- Professional Experience -->
 
-  <h2 class="mt-4 font-serif font-bold sm:text-3xl text-2xl">
+  <h2 class="mt-10 font-serif font-bold sm:text-3xl text-2xl">
     Professional Experience
   </h2>
 
@@ -57,75 +58,42 @@
     title="Co-Founder & (Lead) Full-Stack Developer"
     company="Prisma Safety"
     period="Apr 2021 - Present"
-  >
-    <ul class="ml-8 list-disc sm:max-w-[50vw]">
-      <li>
-        Manage our development team & implement the Prisma App (Flutter), Prisma
+    bullets={[
+      `Manage our development team & implement the Prisma App (Flutter), Prisma
         Websites & Platforms (Svelte/SvelteKit) and all backend infrastructure -
-        public and private facing REST APIs.
-      </li>
-      <li>
-        Develop caching systems using HiveDB to decrease the mobile app's
-        startup time by 5 times.
-      </li>
-      <li>
-        Implement billing services with Stripe and authentication flows using
-        Auth0.
-      </li>
-      <li>
-        Handle CI / CD of our platforms - Front facing platforms on Netlify and
-        backend infra on Heroku / Functions and Firebase or Supabase.
-      </li>
-    </ul>
-  </WorkCard>
+        public and private facing REST APIs.`,
+      `Develop caching systems using HiveDB to decrease the mobile app's
+        startup time by 5 times.`,
+      `Implement billing services with Stripe and authentication flows using
+        Auth0.`,
+      `Handle CI / CD of our platforms - Front facing platforms on Netlify and
+        backend infra on Heroku / Functions and Firebase or Supabase.`,
+    ]}
+  />
 
   <WorkCard
     title="Freelance Web Developer"
     company="Freelancing"
     period="Nov 2020 - Mar 2021"
-  >
-    <ul class="ml-8 list-disc">
-      <li>
-        Design & Implement mobile-first web experiences using Svelte suited to
-        my client's needs.
-      </li>
-      <li>
-        Increased traffic to their platforms by up to 80%, thus improving their
-        long-term revenue and reach.
-      </li>
-      <li>Constantly engaged with my customers to ensure satisfaction.</li>
-    </ul>
-  </WorkCard>
+    bullets={[
+      `Design & Implement mobile-first web experiences using Svelte suited to
+        my client's needs.`,
+      `Increased traffic to their platforms by up to 80%, thus improving their
+        long-term revenue and reach.`,
+      `Constantly engaged with my customers to ensure satisfaction.`,
+    ]}
+  />
 
   <!-- END:Professional Experience -->
 
   <!-- Projects -->
 
-  <h2 class="mt-8 font-serif font-bold sm:text-3xl text-2xl">
+  <h2 class="mt-24 font-serif font-bold sm:text-3xl text-2xl">
     Open Source Projects
   </h2>
 
-  <a
-    class="resume-button"
-    href="https://github.com/davidp-ro"
-    target="_blank"
-    rel="noopener noreferer"
-  >
-    <svg
-      class="h-5 w-5"
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 512 512"
-    >
-      <!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-      <path
-        fill="currentColor"
-        d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h82.7L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3V192c0 17.7 14.3 32 32 32s32-14.3 32-32V32c0-17.7-14.3-32-32-32H320zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z"
-      />
-    </svg>
-    <span class="ml-2"> View GitHub Profile </span>
-  </a>
+  <Projects />
 
-  <!-- END:Projects -->
 </section>
 
 <style lang="postcss">
--- src/lib/components/workCard.svelte
+++ src/lib/components/workCard.svelte
@@ -2,6 +2,7 @@
   export let title: string;
   export let company: string;
   export let period: string;
+  export let bullets: string[];
 </script>
 
 <article class="my-10">
@@ -10,5 +11,10 @@
     <span class="text-accent-300">{company}</span>
     <span class="ml-2 text-sm">{period}</span>
   </div>
-  <slot />
+
+  <ul class="ml-8 list-disc sm:max-w-[50vw]">
+    {#each bullets as bullet}
+      <li>{bullet}</li>
+    {/each}
+  </ul>
 </article>
