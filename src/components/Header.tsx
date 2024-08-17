8d191bc9c0f05dba138190731cdddbc4b1191057
David Pescariu <davidpescariu12@gmail.com>
2024-08-17 18:30:26 +0300

    chore: header cleanup

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,25 +8,20 @@ export default function Header({ baseDelay }: { baseDelay: number }) {
   return (
     <header className="mt-36 mb-12">
       <WordPullUp
-        className="text-6xl font-bold dark:text-white md:text-7xl md:leading-[5rem] text-left"
+        className="font-bold text-left text-6xl md:text-7xl"
         words="Hi there! 👋"
       />
-      <BlurFade
-        className="mt-6 text-xl"
-        delay={baseDelay + NOMINAL_DELAY}
-        inView
-      >
+      <BlurFade className="mt-6 text-xl" delay={baseDelay + NOMINAL_DELAY}>
         I'm David - a software engineer that strives for perfection while
-        building software that scales alongside your business.
+        building products that scale alongside your business.
       </BlurFade>
       <BlurFade
         className="mt-6 text-muted-foreground"
         delay={baseDelay + NOMINAL_DELAY * 2}
-        inView
       >
         <span>
           <b>About:</b> Born and raised in beautiful Romania, I've been coding
-          since I was 14.
+          since I was 13.
           <span className="ml-1 hidden sm:inline">
             I have worked with everything from small microcontrollers to
             large-scale web applications, which is where I currently focus.
@@ -38,7 +33,7 @@ export default function Header({ baseDelay }: { baseDelay: number }) {
         </span>
       </BlurFade>
 
-      <BlurFade className="mt-6" delay={baseDelay + NOMINAL_DELAY * 3} inView>
+      <BlurFade className="mt-6" delay={baseDelay + NOMINAL_DELAY * 3}>
         <CallButton />
       </BlurFade>
     </header>
