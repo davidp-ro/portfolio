56d11af6cba6a6c458beedd33f6584e9b38455e1
David Pescariu <davidpescariu12@gmail.com>
2024-08-17 20:50:06 +0300

    fix: CallButton spacing

--- src/components/CallButton.tsx
+++ src/components/CallButton.tsx
@@ -17,7 +17,7 @@ export default function CallButton({
           onClick={() => openInNewTab("https://cal.com/davidpescariu")}
         >
           <AnimatedShinyText className="inline-flex items-center justify-center px-4 py-1 transition ease-out hover:text-neutral-600 hover:duration-300 hover:dark:text-neutral-400">
-            <span>🪄</span>
+            <span className="mr-0.5">🪄</span>
             {variant === "header" ? (
               <>
                 <span className="ml-1 hidden sm:inline">
