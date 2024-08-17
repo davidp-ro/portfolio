2d6f3240b241a76fc44de52befb79a0e78ca00ac
David Pescariu <davidpescariu12@gmail.com>
2024-08-17 20:18:12 +0300

    feat: open cal link

--- src/components/CallButton.tsx
+++ src/components/CallButton.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { ArrowRightIcon } from "lucide-react";
+import { openInNewTab } from "@/lib/openLink";
+import { ArrowUpRight } from "lucide-react";
 import AnimatedShinyText from "./magicui/animated-shiny-text";
 import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
 
@@ -8,12 +9,17 @@ export default function CallButton() {
   return (
     <div className="w-fit group rounded-full border border-black/5 bg-neutral-100 text-base text-white transition-all ease-in hover:cursor-pointer hover:bg-neutral-200 dark:border-white/5 dark:bg-neutral-900 dark:hover:bg-neutral-800">
       <Tooltip delayDuration={100}>
-        <TooltipTrigger>
+        <TooltipTrigger
+          onClick={() => openInNewTab("https://cal.com/davidpescariu")}
+        >
           <AnimatedShinyText className="inline-flex items-center justify-center px-4 py-1 transition ease-out hover:text-neutral-600 hover:duration-300 hover:dark:text-neutral-400">
             <span>🪄</span>
             <span className="ml-1 hidden sm:inline">Can I help your team?</span>
             <span className="ml-1">Let's jump on a call</span>
-            <ArrowRightIcon className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
+            <ArrowUpRight
+              size={16}
+              className="ml-1 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5"
+            />
           </AnimatedShinyText>
         </TooltipTrigger>
         <TooltipContent sideOffset={10} side="bottom">
