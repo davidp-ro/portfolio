fc0e38bf2d3910cc7a387ceea02f14807c216da5
David Pescariu <davidpescariu12@gmail.com>
2026-07-12 14:00:50 +0300

    feat: resume url redirects

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
+const RESUME_URL =
+  "https://portfolio-files.davidpescariu.com/resume/David%20Pescariu%20-%20Resume.pdf";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      { source: "/cv", destination: RESUME_URL, permanent: false },
+      { source: "/resume", destination: RESUME_URL, permanent: false },
+    ];
+  },
+};
 
 export default nextConfig;
